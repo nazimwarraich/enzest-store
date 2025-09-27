753bb440532b5e353ae0f712003a79ea44505b97
nazimwarraich <nazimwarraich@gmail.com>
2025-09-27 16:08:51 +0500

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,2 @@
+import Head from 'next/head'
+export default function Home(){return (<div style={{padding:20,fontFamily:'Arial'}}><Head><title>Enzest Demo</title></Head><h1>Enzest Demo Store</h1><p>Replace with your storefront content. Open /admin after deployment to manage content.</p></div>)}
